1d01360159409bb5885f42e81238feb28015899e
“RitulV” <rituljv@gmail.com>
2024-11-06 20:12:47 +0530

    updated Token Component

--- Components/HeroSection/HeroSection.jsx
+++ Components/HeroSection/HeroSection.jsx
@@ -75,7 +75,7 @@ const HeroSection = ({ accounts, tokenData }) => {
         )}
       </div>
 
-      {openSetting && <Token openSetting={openSetting} />}
+      {openSetting && <Token setOpenSetting={setOpenSetting} />}
 
       {openToken && (
         <SearchToken
--- Components/Token/Token.jsx
+++ Components/Token/Token.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
 
-const Token = () => {
-  return <div>Token</div>;
-}
+import Style from "./Token.module.css";
+import images from "../../assets";
+import { Toggle } from "../index";
 
-export default Token
+const Token = ({ setOpenSetting }) => {
+  return (
+    <div className={Style.Token}>
+      <div className={Style.Token_box}>
+        <div className={Style.Token_box_heading}>
+          <h4>Setting</h4>
+          <Image
+            src={images.close}
+            alt="close"
+            height={50}
+            width={50}
+            onClick={() => setOpenSetting (false)}
+          />
+        </div>
+
+        <p className={Style.Token_box_para}>
+          Slippage Tolerance{""}
+          <Image src={images.lock} alt="img" height={20} width={20} />
+        </p>
+        <div className={Style.Token_box_input}>
+          <button>Auto</button>
+          <input type="text" placeholder="0.10%" />
+        </div>
+
+        <p className={Style.Token_box_para}>
+          Slippage Tolerance{""}
+          <Image src={images.lock} alt="img" height={20} width={20} />
+        </p>
+        <div className={Style.Token_box_input}>
+          <input type="text" placeholder="30" />
+          <button>minutes</button>
+        </div>
+
+        <h2>Interface Setting</h2>
+        <div className={Style.Token_box_toggle}>
+          <p className={Style.Token_box_para}>Transaction deadline</p>
+          <Toggle />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Token;
