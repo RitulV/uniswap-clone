cdb9f9f3b58dbaafee6ee893c9d0c285703926f7
“RitulV” <rituljv@gmail.com>
2024-11-10 15:20:02 +0530

    entered contract addresses

--- contracts/SwapToken.sol
+++ contracts/SwapToken.sol
@@ -12,10 +12,10 @@ import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 contract SingleSwapToken{
     ISwapRouter public constant swapRouter = ISwapRouter();
 
-    // initialize with actual addresses of the tokens 
-    address public constant DAI;
-    address public constant WETH9;
-    address public constant USDC;
+    // initialize with contract addresses of the tokens 
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     // taken from Uniswap documentation
     // in this function output amount is not fixed
