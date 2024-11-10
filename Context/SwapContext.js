fabf6ce903a81b18b3d4e2b60b057ea3986d26a7
“RitulV” <rituljv@gmail.com>
2024-11-11 00:32:51 +0530

    created swapcontext

---
+++ Context/SwapContext.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from "react";
+import { ethers, BigNumber } from "ethers";
+import Web3Modal from "web3modal";
+
+import {
+  checkIfWalletConnected,
+  connectWallet,
+  connectingWithBooToken,
+  connectingWithLifeToken,
+  connectingWithSingleSwapToken,
+  connectingWithIWETHToken,
+  connectingWithDAIToken,
+} from "../Utils/appFeatures";
+
+import { IWETHABI } from "./constants";
+// import ERC20 from "./ERC20.json";
+
+export const SwapTokenContext = React.createContext();
+
+export const SwapTokenContextProvider = ({ children }) => {
+
+  const [account, setAccount] = useState("");
+  const [ether, setEther] = useState("");
+  const [networkConnect, setNetworkConnect] = useState("");
+  const [weth9, setWeth9] = useState("");
+  const [dai, setDai] = useState("");
+  const [tokenData, setTokenData] = useState([]);
+
+  // weth, boo and life token address
+  const addToken = [
+    "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    "0x90c84237fddf091b1e63f369af122eb46000bc70",
+    "0x3D63c50AD04DD5aE394CAB562b7691DD5de7CF6f",
+  ];
+
+  const fetchingData = async () => {
+    try {
+      // get user account
+      const userAccount = await checkIfWalletConnected();
+      setAccount(userAccount);
+
+      // create provider
+      const web3modal = new Web3Modal();
+      const connection = new web3modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+
+      // check balance
+      const balance = await provider.getBalance(userAccount);
+      const convertBal = BigNumber.from(balance).toString();
+      const ethValue = ethers.utils.formatEther(convertBal);
+        setEther(ethValue);
+        
+        // get network
+        const network = await provider.getNetwork();
+        setNetworkConnect(network.name);
+
+      // all token balance and data
+      addToken.map(async (el, i) => {
+        // getting contract
+        const contract = new ethers.Contract(el, ERC20, provider);
+
+        // getting balance of token
+        const userBalance = await contract.balanceOf(userAccount);
+        const tokenLeft = BigNumber.from(userBalance).toString();
+        const convertTokenBal = ethers.utils.formatEther(tokenLeft);
+
+        // get name and symbol
+        const symbol = await contract.symbol();
+        const name = await contract.name();
+
+        tokenData.push({
+          name: name,
+          symbol: symbol,
+          tokenBalance: convertTokenBal,
+        });
+      });
+
+      //weth balance
+      const wethContract = await connectingWithIWETHToken();
+      const wethBal = await wethContract.balanceOf(userAccount);
+      const wethToken = BigNumber.from(wethBal).toString();
+      const convertWethTokenBal = ethers.utils.formatEther(wethToken);
+      setWeth9(convertWethTokenBal);
+
+      //DAI balance
+      const daiContract = await connectingWithDAIToken();
+      const daiBal = await daiContract.balanceOf(userAccount);
+      const daiToken = BigNumber.from(daiBal).toString();
+      const convertdaiTokenBal = ethers.utils.formatEther(daiToken);
+      setDai(convertdaiTokenBal);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchingData();
+  }, []);
+
+  return (
+    <SwapTokenContext.Provider
+      value={{ account, weth9, dai, networkConnect, ether }}
+    >
+      {children}
+    </SwapTokenContext.Provider>
+  );
+};
--- Context/constants.js
+++ Context/constants.js
@@ -2,20 +2,20 @@ import booToken from "./BooToken.json"
 import lifeToken from "./LifeToken.json"
 import singleSwapToken from "./SingleSwapToken.json";
 import swapMultiHop from "./swapMultiHop.json";
-import IWETH from './IWETH.json '
+import IWETH from './IWETH.json'
   
 // replace with contract addresses
 
-export const BooTokenAddress = "";
+export const BooTokenAddress = "0x90c84237fddf091b1e63f369af122eb46000bc70";
 export const BooTokenABI = booToken.abi; 
 
-export const LifeTokenAddress = "";
+export const LifeTokenAddress = "0x3D63c50AD04DD5aE394CAB562b7691DD5de7CF6f";
 export const LifeTokenABI = lifeToken.abi; 
 
-export const SingleSwapTokenAddress = "";
+export const SingleSwapTokenAddress = "0x103A3b128991781EE2c8db0454cA99d67b257923";
 export const SingleSwapTokenABI = singleSwapToken.abi; 
 
-export const SwapMultiHopAddress = "";
+export const SwapMultiHopAddress = "0xBbc18b580256A82dC0F9A86152b8B22E7C1C8005";
 export const swapMultiHopABI = swapMultiHop.abi; 
 
 
