c7d04668061d9f54725d090f87f19694b1dfa8c4
“RitulV” <rituljv@gmail.com>
2024-11-10 12:38:51 +0530

    wrote deployment scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,33 @@
 const hre = require("hardhat");
 
 async function main() {
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy();
+  // ERC20 BOO Token
+  const BooToken = await hre.ethers.getContractFactory("BooToken");
+  const booToken = await BooToken.deploy();
+  await booToken.deployed();
+  console.log(`BOO deployed to ${booToken.address}`);
 
-  await lock.deployed();
+  // ERC20 LIFE Token
+  const LifeToken = await hre.ethers.getContractFactory("LifeToken");
+  const lifeToken = await LifeToken.deploy();
+  await lifeToken.deployed();
+  console.log(`LIFE deployed to ${lifeToken.address}`);
 
-  console.log(`deployed to ${lock.address}`);
+  // Singleswap Token
+  const SingleSwapToken = await hre.ethers.getContractFactory(
+    "SingleSwapToken"
+  );
+  const singleSwapToken = await SingleSwapToken.deploy();
+  await singleSwapToken.deployed();
+  console.log(`SingleSwapToken deployed to ${singleSwapToken.address}`);
+
+  // Singleswap Token
+  const SwapMultiHop = await hre.ethers.getContractFactory(
+    "SwapMultiHop"
+  );
+  const swapMultiHop = await SwapMultiHop.deploy();
+  await swapMultiHop.deployed();
+  console.log(`SwapMultiHopToken deployed to ${swapMultiHop.address}`);
 }
 
 main().catch((error) => {
